ad8776540cacd45910a32c26d5f832e8d3fc565b
Ali Belbaraka <ali.belbaraka@gmail.com>
2018-10-05 15:33:18 +0200

    added audios samples

--- modules/spectrogram/.ipynb_checkpoints/helpers-checkpoint.py
+++ modules/spectrogram/.ipynb_checkpoints/helpers-checkpoint.py
@@ -1,5 +1,9 @@
 from scipy.signal import correlate
+import numpy as np 
+from scipy.io import wavfile
+import IPython
 import matplotlib.pyplot as plt
+import pyroomacoustics as pra
 
 
 
@@ -25,3 +29,49 @@ def plot_cross(corr):
     fig.set_size_inches(10,4)
     plt.plot(corr)
     plt.title("Correlation")
+
+
+def compute_spectrogram(fs, signal, low_pass=False):
+    
+    
+    #signal = signal[:,0] # mono audio
+    
+    #apply the lowpass filter if needed
+    if low_pass:
+        h_len = 50
+        h = np.ones(h_len)
+        h /= np.linalg.norm(h)
+
+        
+        # stft paramaters 
+        fft_len = 512 
+        block_size = fft_len - 50 + 1
+        hop = block_size // 2
+        window = pra.hann(block_size, flag='asymmetric', length='full') 
+
+        stft = pra.realtime.STFT(block_size, hop=hop, analysis_window=window, channels=1)
+        stft.set_filter(h, zb=h.shape[0] - 1)  
+        processed_audio = np.zeros(signal.shape)
+        n = 0
+        while  signal.shape[0] - n > hop:
+            stft.analysis(signal[n:n+hop,])
+            stft.process()  # apply the filter
+            processed_audio[n:n+hop,] = stft.synthesis()
+            n += hop
+            
+        fig = plt.figure()
+        fig.set_size_inches(20,8)
+        plt.subplot(2,1,1)
+        spec1, _, _, _ = plt.specgram(signal[:n-hop].astype(np.float32), NFFT=256, Fs=fs, vmin=-20, vmax=30) # dBFS scale
+        plt.title('Original Signal', fontsize=22)
+        plt.subplot(2,1,2)
+        spec2, _, _, _ = plt.specgram(processed_audio[hop:n], NFFT=256, Fs=fs, vmin=-20, vmax=30) # dBFS scale
+        plt.title('Lowpass Filtered Signal', fontsize=22)
+        return spec1, spec2
+    else:
+        fig = plt.figure()
+        fig.set_size_inches(20,5)
+        spec, _, _, _ = plt.specgram(signal.astype(np.float32), NFFT=256, Fs=fs, vmin=-20, vmax=30) # dBFS scale
+        plt.title('Spectrogram', fontsize=22)
+        return spec
+    
